89dc43bd2739b5dc42279906e61c1b4c7d6497ac
lambhunter0 <george.theophanous@hotmail.com>
2018-09-20 19:50:41 -0400

    Country
    initial commit with country.cpp and country.h

---
+++ Risk_COMP345/Risk_COMP345/Country.cpp
@@ -0,0 +1,105 @@
+#include "Country.h"
+
+Country::Country() {
+	countryName = "";
+	numberOfArmies = 0;
+}
+
+Country::Country(const string& countryName1) {
+	countryName = countryName1;
+	numberOfArmies = 0;
+}
+
+Country::Country(const string& countryName1, const int& i) {
+	countryName = countryName1;
+	numberOfArmies = i;
+}
+
+Country::Country(const string& countryName1, Player* p, const int& i) {
+	countryName = countryName1;
+	owner = p;
+	numberOfArmies = i;
+}
+
+Country::~Country() {
+}
+
+void Country::setCountryName(const string& countryName1) {
+	countryName = countryName1;
+}
+
+string Country::getCountryName() {
+	return countryName;
+}
+
+void Country::addNeighbor(Country* country) {
+	neighbors.push_back(country);
+}
+
+Country* Country::getNeighbor(const int& index) {
+	return neighbors[index];
+}
+
+vector<Country*> Country::getNeighbors() {
+	return neighbors;
+}
+
+bool Country::isNeighborOf(Country* country)
+{
+	if (neighbors.empty())
+	{
+		return false;
+	}
+
+	vector<Country*>::iterator it = find(neighbors.begin(), neighbors.end(), country);
+
+	return it != neighbors.end();
+}
+
+void Country::setNumberOfArmies(const int& ammount) {
+	numberOfArmies = ammount;
+}
+
+int Country::getNumberOfArmies() {
+	return numberOfArmies;
+}
+
+void Country::setOwner(Player* player) {
+	owner = player;
+}
+
+Player* Country::getOwner() {
+	return owner;
+}
+
+bool Country::BelongsToAContinent()
+{
+	return continent != NULL;
+}
+
+Continent* Country::GetContinent() const
+{
+	return continent;
+}
+
+void Country::SetContinent(Continent *continent1)
+{
+	if (continent == NULL)
+	{
+		continent = continent1;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Risk_COMP345/Risk_COMP345/Country.h
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+class Continent;
+using std::string;
+using std::vector;
+#ifndef COUNTRY_H_
+#define COUNTRY_H_
+
+class Player;
+
+class Country {
+
+private:
+	string countryName;
+	int numberOfArmies;
+	vector<Country*> neighbors;
+	Player* owner;
+	Continent* continent;
+
+public:
+	Country();
+	Country(const string& countryName);
+	Country(const string& countryName, const int& numberOfArmies);
+	Country(const string& countryName, Player* p, const int& numberOfArmies);
+	~Country();
+	void setCountryName(const string& name);
+	string getCountryName();
+	void addNeighbor(Country* country);
+	vector<Country*> getNeighbors();
+	Country* getNeighbor(const int& index);
+	bool isNeighborOf(Country* country);
+	void setNumberOfArmies(const int& ammount);
+	int getNumberOfArmies();
+	void setOwner(Player* player);
+	Player* getOwner();
+	bool BelongsToAContinent();
+	Continent* GetContinent() const;
+	void SetContinent(Continent* continent);
+};
+
+#endif;
+
+
+
