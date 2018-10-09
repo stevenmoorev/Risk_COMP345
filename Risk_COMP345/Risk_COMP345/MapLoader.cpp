a0250cecb332365685cba1752d70f76e1a7b4d9c
pcabro <i_rozyba@encs.concordia.ca>
2018-10-08 23:28:45 -0400

    fixed the bugs

--- Risk_COMP345/Risk_COMP345/MapLoader.cpp
+++ Risk_COMP345/Risk_COMP345/MapLoader.cpp
@@ -39,7 +39,12 @@ Map Load() {
 	}
 	cout << continents.size();
 	cout << "\n";
-	//skip 2 unecessary lines
+	
+	vector<Country*> countries;
+	vector<Country*> neighbors;
+	int c = 0;
+	int o = 0;
+	//skip 2 unecessary 
 	getline(in, junk, '\n');
 	getline(in, junk, '\n');
 	while (!in.eof())
@@ -47,81 +52,170 @@ Map Load() {
 		if (in.peek() != flag && in.peek() != -1)
 		{
 			getline(in, countryName, ',');
-			Country country;
-			country.setCountryName(countryName);
-			cout << country.getCountryName();
-			cout << "\n";
-			in >> x;
-			country.setX(x);
-			in.get();
-			in >> y;
-			country.setY(y);
-			cout << country.getY();
-			in.get();
-			cout << "\n";
-			//country.SetContinent(continent);
-			//find continent that country belongs to in the continents vector
-			//country.SetContinent(desired continent)
+			//Country country;
+			//country.setCountryName(countryName);
+			//cout << country.getCountryName();
 
-			//vector<Country*> Country.SetContient(Continent);
-			getline(in, continentName, ',');
-			
-			for (unsigned int i = 0; i < continents.size(); i++)
+			auto it = std::find(neighbors.begin(), neighbors.end(), new Country(countryName));
+			//if the country is not already created in the neighbor vector
+			if (it == neighbors.end())
 			{
-				if (continentName == continents[i]->GetName())
-				{
-					cout << "\n";
-					cout << "Continent name ";
-					cout << continents[i]->GetName();
-					cout << "Continent SIZE ";
-					cout << continents.size();
-					cout << "\n";
-				//	country.SetContinent((continents[i]));
-					continents[i]->AddCountry(&country);
-					//cout << country.BelongsToAContinent();
-					cout << continents[i]->ContainsCountry(&country);
-					cout << continents[i]->GetCountryCount() + "HERE \n";
-				}
-			}
-//********************************setting neighbors, BUT SEEMS TO BE NOT WORKING PERFECTLY
-			getline(in, countryName, flag);
-			std::istringstream string(countryName);
-			std::string neighbor;
-			while (std::getline(string, neighbor, ',')) {
-				cout << neighbor;
+				countries.push_back(new Country(countryName));
+				//countries[c]
+				cout << countries[c]->getCountryName();
 				cout << "\n";
-				Country country1;
-				country1.setCountryName(neighbor);
-				country.addNeighbor(&country1);
-				//}
-				//if(continentName == continent.getName()){   country.setContinent(continent)     }
+				in >> x;
+				//country.setX(x);
+				countries[c]->setX(x);
+				in.get();
+				in >> y;
+				countries[c]->setY(y);
+				//country.setY(y);
+				//cout << country.getY();
+				in.get();
+				cout << "\n";
+				//country.SetContinent(continent);
+				//find continent that country belongs to in the continents vector
+				//country.SetContinent(desired continent)
+
+				//vector<Country*> Country.SetContient(Continent);
+				getline(in, continentName, ',');
+				//Map map;
+				for (unsigned int i = 0; i < continents.size(); i++)
+				{
+					if (continentName == continents[i]->GetName())
+					{
+						cout << "\n";
+						cout << "Continent name ";
+						cout << continents[i]->GetName();
+						cout << "Continent SIZE + object ID: ";
+						cout << continents[i];
+						//continents[i]->SetMap(&map);
+						cout << "\n";
+						//	country.SetContinent((continents[i]));
+						continents[i]->AddCountry(countries[c]);
+						cout << "Country memory address:" << countries[c];
+						//cout << "Country memory address:" << &country;
+						//cout << country.BelongsToAContinent();
+						// country count =1 ????????
+						cout << continents[i]->ContainsCountry(countries[c]);
+						//	cout << continents[i]->ContainsCountry(&country);
+						cout << continents[i]->GetCountryCount() + " HERE \n";
+					}
+				}
+				//********************************setting neighbors, BUT SEEMS TO BE NOT WORKING PERFECTLY
+				getline(in, countryName, flag);
+				std::istringstream string(countryName);
+				std::string neighbor;
+				while (std::getline(string, neighbor, ',')) {
+					cout << "neigbor: ";
+					cout << neighbor;
+					cout << "\n";
+					neighbors.push_back(new Country(neighbor));
+					//Country country1;
+					neighbors[o]->setCountryName(neighbor);
+					countries[c]->addNeighbor(neighbors[o]);
+					o = o + 1;
+					//country.addNeighbor(&country1);
+					//}
+					//if(continentName == continent.getName()){   country.setContinent(continent)     }
+				}
+				cout << "\n";
+				c = c + 1;
 			}
-			cout << "\n";
-		}
-		else
+
+			else {
+				auto index = std::distance(neighbors.begin(), it);
+				cout << neighbors[index]->getCountryName();
+				cout << "\n";
+				in >> x;
+				//country.setX(x);
+				neighbors[index]->setX(x);
+				in.get();
+				in >> y;
+				neighbors[index]->setY(y);
+				//country.setY(y);
+				//cout << country.getY();
+				in.get();
+				cout << "\n";
+				//country.SetContinent(continent);
+				//find continent that country belongs to in the continents vector
+				//country.SetContinent(desired continent)
+
+				//vector<Country*> Country.SetContient(Continent);
+				getline(in, continentName, ',');
+				//Map map;
+				for (unsigned int i = 0; i < continents.size(); i++)
+				{
+					if (continentName == continents[i]->GetName())
+					{
+						cout << "\n";
+						cout << "Continent name ";
+						cout << continents[i]->GetName();
+						cout << "Continent SIZE + object ID: ";
+						cout << continents[i];
+						//continents[i]->SetMap(&map);
+						cout << "\n";
+						//	country.SetContinent((continents[i]));
+						continents[i]->AddCountry(neighbors[index]);
+						cout << "Country memory address:" << countries[c];
+						//cout << "Country memory address:" << &country;
+						//cout << country.BelongsToAContinent();
+						// country count =1 ????????
+						cout << continents[i]->ContainsCountry(neighbors[index]);
+						//	cout << continents[i]->ContainsCountry(&country);
+						cout << continents[i]->GetCountryCount() + " HERE \n";
+					}
+				}
+				//********************************setting neighbors, BUT SEEMS TO BE NOT WORKING PERFECTLY
+				getline(in, countryName, flag);
+				std::istringstream string(countryName);
+				std::string neighbor;
+				while (std::getline(string, neighbor, ',')) {
+					cout << "neigbor: ";
+					cout << neighbor;
+					cout << "\n";
+					neighbors.push_back(new Country(neighbor));
+					//Country country1;
+					neighbors[o]->setCountryName(neighbor);
+					neighbors[index]->addNeighbor(neighbors[o]);
+					o = o + 1;
+					//country.addNeighbor(&country1);
+					//}
+					//if(continentName == continent.getName()){   country.setContinent(continent)     }
+				}
+				cout << "\n";
+			}
+			}
+		 else 
 		{
 			in.get();
 		}
-		
+			
 	}
+
 	Map map;
 	for (unsigned int i = 0; i < continents.size(); i++)
 	{
 		{	
-			cout << continents[i]->GetCountryCount() + "\n";
+			//cout << continents[i]->GetCountryCount() + "\n";
 			map.AddContinentToMap(continents[i]);
 			//cout << "FLAG1";
-			cout << "\n";
+			//cout << "\n";
 		}
 	}
 	cout << map.GetContinentCount();
+	cout << "\n";
 	//vector<Country*>Map2= map.GetAllCountries();
-//****************************************trying to verify how many countries are part of each continent which currently gives 1 
+	//****************************************trying to verify how many countries are part of each continent which currently gives 1 
 	for (unsigned int x = 0; x < map.GetMapContinents().size(); x++) {
 		cout << map.GetMapContinents()[x]->GetCountryCount();
 		cout << "\n";
-		//cout << "FLAG + \n";
+		cout << "FLAG2 + \n";
 	}
+	cout << continents[1]->ContainsCountry(countries[7]);
+	cout << continents[7]->ContainsCountry(countries[40]);
+	cout << countries[0]->isNeighborOf(neighbors[4]);
 	//vector<Country*> GetCountries() const;
 	system("pause");
 	return map;
